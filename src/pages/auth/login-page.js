091e6e75b48f7eee9a8bc44a1356cdf83ec79730
Yassine Rassy <yassine.rassy1@gmail.com>
2021-03-14 22:41:00 +0100

    still working on the login width firebase in progress ...

--- src/pages/auth/login-page.js
+++ src/pages/auth/login-page.js
@@ -1,10 +1,58 @@
 import React, { Component } from "react";
 import Login from "../../components/auth/login";
+import { auth } from "../../utils/firebase";
 
 import "./auth.css";
 
 export default class LoginPage extends Component {
-  render() {
-    return <Login></Login>;
+
+  constructor(){
+
+    super()
+
+    this.state = {
+      username:"",
+      password:""
+    }
   }
+  render() {
+    return (
+      <Login handleChange={this.changeInput} handleSubmit={this.signIn} />
+    );
+  }
+
+  //----validate form
+  formIsValid = () => {
+    if (
+      this.state.username == "" ||
+      this.state.password == ""
+    ) {
+      alert(" All Fields are required 😋 !!");
+      return false;
+    }
+      return true;
+    }
+  };
+
+  //---- signin
+  signin = (e) => {
+
+    e.preventDefault();
+
+    if (this.formIsValid() == true) {
+
+      auth.signInWithEmailAndPassword(this.state.username,this.state.password)
+      .then((response) => {})
+      .catch((error) => {});
+    }
+  };
+
+  //-----handle input
+  changeInput = (e) => {
+    let value = e.target.value;
+    let name = e.target.name;
+    this.setState({
+      [name]: value,
+    });
+  };
 }
