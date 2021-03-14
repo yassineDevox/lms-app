902dbdc22e00358936558db4a301ccac9012b3ad
Yassine Rassy <yassine.rassy1@gmail.com>
2021-03-14 22:32:22 +0100

    still working on register using auth firebase

--- src/components/auth/register.jsx
+++ src/components/auth/register.jsx
@@ -1,66 +1,84 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 
-
 export default function Register() {
-    return (
-
-        <div className="container">
-        <div className="row">
-          <div className="col-lg-3 col-md-2" />
-          <div className="col-lg-6 col-md-8 login-box">
-            <div className="col-lg-12 login-key">
-              <i className="fas fa-sign-in-alt" aria-hidden="true" />
-            </div>
-            <div className="col-lg-12 login-title">ADMIN PANEL</div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-3 col-md-2" />
+        <div className="col-lg-6 col-md-8 login-box">
+          <div className="col-lg-12 login-key">
+            <i className="fas fa-sign-in-alt" aria-hidden="true" />
+          </div>
+          <div className="col-lg-12 login-title">ADMIN PANEL</div>
+          <div className="col-lg-12 login-form">
             <div className="col-lg-12 login-form">
-              <div className="col-lg-12 login-form">
-                <form>
-                  <div className="form-group">
-                    <label className="form-control-label">LASTNAME</label>
-                    <input type="text" className="form-control" />
-                  </div>{" "}
-                  <div className="form-group">
-                    <label className="form-control-label">FIRSTNAME</label>
-                    <input type="text" className="form-control" />
-                  </div>{" "}
-                  <div className="form-group">
-                    <label className="form-control-label">EMAIL ADRESS</label>
-                    <input type="text" className="form-control" />
-                  </div>
-                  <div className="form-group">
-                    <label className="form-control-label">PASSWORD</label>
-                    <input type="password" className="form-control" i />
-                  </div>
-                  <div className="form-group">
-                    <label className="form-control-label">
-                      REPEAT PASSWORD
-                    </label>
-                    <input type="password" className="form-control" i />
-                  </div>
-                  <div className="loginbttm">
-                    <div className="d-flex justify-content-between align-items-center login-button ">
-                      <Link to="/">
-                       <span className="text-white">
+              <form onSubmit={props.handleSubmit}>
+                <div className="form-group">
+                  <label className="form-control-label">LASTNAME</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    onChange={props.handleChange}
+                    name="lastname"
+                  />
+                </div>{" "}
+                <div className="form-group">
+                  <label className="form-control-label">FIRSTNAME</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    onChange={props.handleChange}
+                    name="firstname"
+                  />
+                </div>{" "}
+                <div className="form-group">
+                  <label className="form-control-label">EMAIL ADRESS</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    onChange={props.handleChange}
+                    name="username"
+                  />
+                </div>
+                <div className="form-group">
+                  <label className="form-control-label">PASSWORD</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    onChange={props.handleChange}
+                    name="password"
+                  />
+                </div>
+                <div className="form-group">
+                  <label className="form-control-label">REPEAT PASSWORD</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    onChange={props.handleChange}
+                    name="repeatedPassword"
+                  />
+                </div>
+                <div className="loginbttm">
+                  <div className="d-flex justify-content-between align-items-center login-button ">
+                    <Link to="/">
+                      <span className="text-white">
                         {" "}
                         Already have an account ?
                       </span>
-                      </Link>
-                     
-                      <button
-                        type="submit"
-                        className="btn btn-outline-warning "
-                      >
-                        Register
-                      </button>
-                    </div>
+                    </Link>
+
+                    <button type="submit" className="btn btn-outline-warning ">
+                      Register
+                    </button>
                   </div>
-                </form>
-              </div>
+                </div>
+              </form>
             </div>
-            <div className="col-lg-3 col-md-2" />
           </div>
+          <div className="col-lg-3 col-md-2" />
         </div>
       </div>
-    )
+    </div>
+  );
 }
--- src/pages/auth/register-page.js
+++ src/pages/auth/register-page.js
@@ -1,19 +1,74 @@
 import React, { Component } from "react";
 import Register from "../../components/auth/register";
+import { auth } from "../../utils/firebase";
 
 import "./auth.css";
 
 export default class RegisterPage extends Component {
-  
-  constructor(){
-    
+  constructor() {
+    super();
+
+    this.state = {
+      username: "",
+      password: "",
+      repeatedPassword: "",
+      firstname: "",
+      lastname: "",
+    };
   }
-  
+
   render() {
-    return <Register></Register>;
+    return (
+      <Register
+        handleChange={this.changeInput}
+        handleSubmit={this.signup}
+      ></Register>
+    );
   }
 
+  //----validate form
+  formIsValid = () => {
+    if (
+      this.state.firstname == "" ||
+      this.state.lastname == "" ||
+      this.state.username == "" ||
+      this.state.password == "" ||
+      this.state.repeatedPassword
+    ) {
+      alert(" All Fields are required 😋 !!");
+      return false;
+    }
+    else if (this.state.password!=this.state.repeatedPassword){
+      alert(" password and password repeated not match 🧐 !!")
+      return false;
+    }
+    else {
+      return true;
+    }
+  };
 
+  //---- signup
+  signup = (e) => {
+    
+    e.preventDefault();
 
+    if(this.formIsValid()==true){
+      auth.createUserWithEmailAndPassword(this.state.username,this.state.password)
+      .then((response)=>{
+          
+      }).catch((error)=>{
 
+      })
+    }
+
+  };
+
+  //-----handle input
+  changeInput = (e) => {
+    let value = e.target.value;
+    let name = e.target.name;
+    this.setState({
+      [name]: value,
+    });
+  };
 }
