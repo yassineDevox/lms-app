98f6c0428991b08a02ae57a3bedb5a8df14e45c8
Yassine Rassy <yassine.rassy1@gmail.com>
2021-03-15 17:03:37 +0100

    dazdazd

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -57,7 +57,34 @@ class Home extends React.Component {
             handleEditFromHome={this.editStudent}
             handleFilterFromHome={this.filterStudentsByName}
           />
+
         </div>
+        
+<div>
+  {/* Button trigger modal */}
+  <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Launch demo modal
+  </button>
+  {/* Modal */}
+  <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div className="modal-dialog">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+        </div>
+        <div className="modal-body">
+          ...
+        </div>
+        <div className="modal-footer">
+          <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" className="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
       </>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import  MyApp from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+
 ReactDOM.render(
   <React.StrictMode>
     <MyApp />
